25974a2a1c8ba565a8edb223325ac38aee7f97cc
teju7575 <tejaswitengale@gmail.com>
2025-03-25 13:14:03 +0530

    1
    1

---
+++ SameReturnType.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(a int) (int, int) {
+	return a / 2, a * 2
+}
+
+func main() {
+	x, y := add(16)
+	fmt.Println(x, y)
+}
+{func main ()
+	 x, y:= add
+	 fmt.printIn(x, y)
+	 
